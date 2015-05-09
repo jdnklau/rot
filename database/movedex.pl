4d0c1d53b5c8c7f45ed817cb48e894ce62c0324d
jdnklau <jannik.dunkelau@hhu.de>
2015-05-09 16:56:18 +0200

    Updated Movedex
    Movedex now contains information about number of hits and whether or not
    the move causes contact

--- database/movedex.pl
+++ database/movedex.pl
@@ -1,26 +1,5 @@
-move('aerial ace', flying, physical(60), acc(always),pp(20), prio(0), noeffect).
-move('air slash', flying, special(75), acc(95), pp(15), prio(0), effect(flinch(30))).
-move('aura sphere', fighting, special(80), acc(always), pp(20), prio(0), noeffect).
-move('brave bird', flying, physical(120), acc(100), pp(15), prio(0), effect(recoil(33))).
-move('dark pulse', dark, special(80), acc(100), pp(15), prio(0), effect(flinch(20))).
-move('dragon pulse', dragon, special(85), acc(100), pp(10), prio(0), noeffect).
-move('double hit', normal, physical(35), acc(90), pp(10), prio(0), effect(multistrike(2))).
-move('earth power', ground, special(90), acc(100), pp(10), prio(0), effect(spd(target, 10, -1))).
-move(earthquake, ground, physical(100), acc(100), pp(10), prio(0), noeffect).
-move('energy ball', grass, special(90), acc(100), pp(10), prio(0), effect(spd(target, 10, -1))).
-move('fake out', normal, physical(40), acc(100), pp(10), prio(3), effect(flinch(100), just_in)).
-move(flamethrower, fire, special(90), acc(100), pp(15), prio(0), effect(burn(10))).
-move('flash canon', steel, special(80), acc(100), pp(10), prio(0), effect(spd(target, 10, -1))).
-move('knock off', dark, physical(65), acc(100), pp(25), prio(0), effect(knockoff)).
-move('lava plume', fire, special(80), acc(100), pp(15), prio(0), effect(burn(30))).
-move(moonblast, fairy, special(95), acc(100), pp(15), prio(0), effect(spa(target, 30, -1))).
-move(protect, normal, status, acc(always), pp(10), prio(0), effect(protect)).
-move('rapid spin', normal, physical(20), acc(100), pp(40), prio(0), effect(spin)).
-move('roost', flying, status, acc(always), pp(10), prio(0), effect(heal(50), rooststate)).
-move(scald, water, special(80), acc(100), pp(15), prio(0), effect(burn(30))).
-move('sludge wave', poison, special(95), acc(100), pp(10), prio(0), effect(poison(10))).
-move(spikes, ground, status, acc(always), pp(20), prio(0), effect(spikes)).
-move('stealth rock', rock, status, acc(always), pp(20), prio(0), effect(stealthrock)).
-move(toxic, poison, status, acc(90), pp(10), prio(0), effect(tox(100))).
-move('u-turn', bug, physical(70), acc(100), pp(20), prio(0), effect(switchuser)).
-move(wish, normal, status, acc(always), pp(10), prio(0), effect(wish)).
+move('aerial ace', flying, physical(60), acc(always),pp(20), prio(0), contact, 1, noeffect).
+move('aura sphere', fighting, special(80), acc(always), pp(20), prio(0), nocontact, 1, noeffect).
+move('dragon pulse', dragon, special(85), acc(100), pp(10), prio(0), nocontact, 1, noeffect).
+move('double hit', normal, physical(35), acc(90), pp(10), prio(0), contact, 2, noeffect).
+move(earthquake, ground, physical(100), acc(100), pp(10), prio(0), nocontact, 1, noeffect).
