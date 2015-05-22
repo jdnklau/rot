c3cd9b5d1876cbae77797bcca27c599284e76dfe
jdnklau <jannik.dunkelau@hhu.de>
2015-05-22 16:39:31 +0200

    Added Available Moves Predicate
    Added predicates to list available moves or switches

--- observing_predicates.pl
+++ observing_predicates.pl
@@ -11,3 +11,20 @@ primary_status_condition([_,_,_,_,_,[Condition|_]], Condition) :-
 
 hp_percent([_, kp(Curr, Max)|_], P) :-
   P is Curr / Max * 100.
+
+
+available_switches([_|Team], Available) :-
+  available_switches_acc(Team, [], Available).
+available_switches_acc([Pokemon|Rest], List, Available) :-
+  \+ fainted(Pokemon),
+  Pokemon = [Name|_],
+  available_switches_acc(Rest, [switch(Name)|List], Available).
+available_switches_acc([Pokemon|Rest], List, Available) :-
+  fainted(Pokemon),
+  available_switches_acc(Rest, List, Available).
+available_switches_acc([], Available, Available).
+
+available_moves([Lead|Team], [M1, M2, M3, M4|Available_switches]) :-
+  Lead = [_, _, Moves|_],
+  Moves = [[M1,_], [M2,_], [M3,_], [M4,_]],
+  available_switches([Lead|Team], Available_switches).
