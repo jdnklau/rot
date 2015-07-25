22f7958b88f3008286386b074767eff9c202fc9e
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2015-07-25 18:16:01 +0200

    Added Setup Predicate for Status Value Frames
    There is now a predicate set_up_stats/4 expecting a base stat frame and a ev/dv distribution as input arguments and returning the resulting hit points and the resulting status value frame (excluding hit points).
    
    Makes the code for creating a status frame more reusable as it has now it's very own predicate instead of having to use set_up_stat/3 5 times.

--- tools/setups.pl
+++ tools/setups.pl
@@ -41,6 +41,11 @@ stat_stage_increase_message(Name, Stat, _, Increase, [target(stat_stage(pokemon(
 % Sets up a pokemon by its wished attributes an translates it to the pokemon data structure
 % used in the program.
 %
+% The EV and DV data is a six-tupel containing six tupels corresponding to the status values.
+% Those tupels are of the form (Effort_value, Determinant_value).
+% Effort values range from 0 to 252, but the sum of all six of them does not exceed 510.
+% Determinant values range from 0 to 31 and are each independend of the other's base stat DV.
+%
 % @arg Name The name of the pokemon
 % @arg Nature The nature of the pokemon
 % @arg Ability The ability of the pokemon
@@ -48,26 +53,36 @@ stat_stage_increase_message(Name, Stat, _, Increase, [target(stat_stage(pokemon(
 % @arg EV_DV_data Information about how the EV and DV of the pokemon are spent
 % @arg Item the item to be hold
 % @arg Result The resulting pokemon data
-% @tbd implement the consultation of ev and dv values
 set_up_pokemon(Name, Nature, Ability, [M1, M2, M3, M4], EV_DV, Item, Result) :-
   set_up_move(M1, Move_1),
   set_up_move(M2, Move_2),
   set_up_move(M3, Move_3),
   set_up_move(M4, Move_4),
   Moves = [Move_1, Move_2, Move_3, Move_4],
-  pokemon(Name, Types, stats(B_KP, B_Atk, B_Def, B_Spa, B_Spd, B_Ini),_), % get base stats
-  EV_DV = (Hp_ed, Atk_ed, Def_ed, Spa_ed, Spd_ed, Spe_ed), % access ev/dv
-  set_up_kp(B_KP, Hp_ed, KP),
-  set_up_stat(B_Atk, Atk_ed, Raw_Atk),
-  set_up_stat(B_Def, Def_ed, Raw_Def),
-  set_up_stat(B_Spa, Spa_ed, Raw_Spa),
-  set_up_stat(B_Spd, Spd_ed, Raw_Spd),
-  set_up_stat(B_Ini, Spe_ed, Raw_Ini),
-  apply_nature(Nature,Raw_Atk, Raw_Def, Raw_Spa, Raw_Spd, Raw_Ini, Atk, Def, Spa, Spd, Ini),
+  pokemon(Name, Types, Base_stats,_), % get base stats
+  set_up_stats(Base_stats, EV_DV, Hp, Stats),
+  apply_nature(Nature,Stats, Stats_nature),
   Result =
-    [Name, kp(KP, KP), Moves,
-      [Ability, stats(Atk, Def, Spa, Spd, Ini), Types, stat_stages(0,0,0,0,0), EV_DV],
+    [Name, kp(Hp, Hp), Moves,
+      [Ability, Stats_nature, Types, stat_stages(0,0,0,0,0), EV_DV],
       Item, [nil, [], []]], !.
+
+%! set_up_stats(+Base_stat_frame, +EV_DV_data, -HP, -Stat_frame).
+% Calculates the resulting stat frame by base stats and ev/dv distribution
+% @arg Base_stat_frame A stat/6 frame containing the base status values (including hit points)
+% @arg EV_DV_data The ev/dv distribiution
+% @arg HP The maximal hit points by the given base stats and ev/dv
+% @arg Stat_frame a stat/5 frame containing the resulting status values (excluding hit points)
+set_up_stats(Base_stats, EV_DV, Hp, Stats) :-
+  Base_stats = stats(B_Hp, B_Atk, B_Def, B_Spa, B_Spd, B_Spe),
+  EV_DV = (Hp_ed, Atk_ed, Def_ed, Spa_ed, Spd_ed, Spe_ed), % access ev/dv
+  set_up_kp(B_Hp, Hp_ed, Hp),
+  set_up_stat(B_Atk, Atk_ed, Atk),
+  set_up_stat(B_Def, Def_ed, Def),
+  set_up_stat(B_Spa, Spa_ed, Spa),
+  set_up_stat(B_Spd, Spd_ed, Spd),
+  set_up_stat(B_Spe, Spe_ed, Spe),
+  Stats = stats(Atk,Def,Spa,Spd,Spe).% set up result
 
 %! set_up_kp(+Base_value, +EV_DV_data, -Resulting_value)
 % Calculate the correct stat value from the base value
@@ -87,7 +102,7 @@ set_up_stat(Base_value, (EV,DV), Stat_value) :-
   DV_and_EV is DV + EV//4,
   Stat_value is (2*Base_value + DV_and_EV)/2 + 5.
 
-apply_nature(_,Atk,Def,Spa,Spd,Ini,Atk,Def,Spa,Spd,Ini). % NYI
+apply_nature(_,Stats,Stats). % NYI
 
 %! set_up_move(+Move_name, -Move_data)
 %
