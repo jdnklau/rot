8c3b8497d4df299f4cb8a095f5471094f5bb75f8
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2015-08-11 17:51:22 +0200

    Added Hit Point Evaluation

--- rot/expert/data_evaluation.pl
+++ rot/expert/data_evaluation.pl
@@ -144,7 +144,7 @@ rot_evaluate_ev_dv(A1) :-
   rot_init_ev_dv_vars(EV_DV, EV_DV_vars),
   EV_DV_vars = ((Hp_ed, Hp_dd),(Atk_ed, Atk_dd),(Def_ed, Def_dd),(Spa_ed,Spa_dd),(Spd_ed,Spd_dd),(Spe_ed,Spe_dd)),
   % get status value domains
-  hp_frame(Pokemon, kp(Hp_curr_d,Hp_d)),
+  hp_frame(Pokemon, kp(_,Hp_d)),
   stats(Pokemon, Atk_d, Def_d, Spa_d, Spd_d, Spe_d),
   % status value constraint vars
   Hp in Hp_d,
@@ -156,20 +156,21 @@ rot_evaluate_ev_dv(A1) :-
   % get base values
   pokemon(A1,_,stats(Hp_b,Atk_b,Def_b,Spa_b,Spd_b,Spe_b),_),
   % evaluate stats
+  rot_evaluate_hp_stat(Hp_b,Hp,Hp_ed,Hp_dd),
   rot_evaluate_stat(Atk_b,Atk,Atk_ed,Atk_dd),
   rot_evaluate_stat(Def_b,Def,Def_ed,Def_dd),
   rot_evaluate_stat(Spa_b,Spa,Spa_ed,Spa_dd),
   rot_evaluate_stat(Spd_b,Spd,Spd_ed,Spd_dd),
   rot_evaluate_stat(Spe_b,Spe,Spe_ed,Spe_dd),
   % get new domains - ev
-  fd_dom(Hp_ed, Hp_ned), % FIXME Add hp calculations
+  fd_dom(Hp_ed, Hp_ned),
   fd_dom(Atk_ed,Atk_ned),
   fd_dom(Def_ed,Def_ned),
   fd_dom(Spa_ed,Spa_ned),
   fd_dom(Spd_ed,Spd_ned),
   fd_dom(Spe_ed,Spe_ned),
   % get new domains - dv
-  fd_dom(Hp_ed, Hp_ndd), % FIXME Add hp calculations
+  fd_dom(Hp_dd, Hp_ndd),
   fd_dom(Atk_dd,Atk_ndd),
   fd_dom(Def_dd,Def_ndd),
   fd_dom(Spa_dd,Spa_ndd),
@@ -181,17 +182,28 @@ rot_evaluate_ev_dv(A1) :-
   set_ev_dv_data(Pokemon,New_ev_dv_domains,New_pokemon),
   rot_update_known_pokemon(New_pokemon).
 
-%! rot_evaluate_stat(+Base_stat,+Status_value,+EV,+DV).
+%! rot_evaluate_hp_stat(+Base_value,+Maximal_value,+EV,+DV).
+% Connects the given constraint domain variables by the hit point status value formula,
+% thus reducing the domain of the given variables even further.
+% @arg Base_value The hit point's base value
+% @arg Maximal_value A constraint variable in the possible range of maximal hit points
+% @arg EV A constraint variable in the possible range of ev
+% @arg DV A constraint variable in the possible range of dv
+rot_evaluate_hp_stat(Base, Stat, EV, DV) :-
+  % use formula
+  Stat #= (2*Base + DV + EV/4 + 100)/2 + 10.
+
+%! rot_evaluate_stat(+Base_value,+Status_value,+EV,+DV).
 %
 % Connects the given constraint domain variables by the status value formula, thus
 % reducing the domains of the given variables even further.
 %
 % Not to be used for the pokemons hit points
 %
-% @arg Base_stat The stat's base value
-% @arg Status_value A constraind variable in the possible range of status values
-% @arg EV A constraind variable in the possible range of ev
-% @arg DV A constraind variable in the possible range of dv
+% @arg Base_value The stat's base value
+% @arg Status_value A constraint variable in the possible range of status values
+% @arg EV A constraint variable in the possible range of ev
+% @arg DV A constraint variable in the possible range of dv
 rot_evaluate_stat(Base,Stat,EV,DV) :-
   % just set a another constraint
   Stat #= (2*Base + DV + EV/4)/2 + 5.
